6324316b3dee915ed0b981cf7b3082e8a18cf326
subasri19 <subasrigovindarajan@gmail.com>
2018-08-02 00:36:25 +0530

    Initial commit

--- signup.js
+++ signup.js
@@ -1,3 +1,18 @@
 $(document).ready(function() {
-
+    $("#signup").hide();
+    $("h1").lettering();
 });
+
+function showSignup() {
+    $(".signupTitle").click(function() {
+        $("#login").hide();
+        $("#signup").show();
+    });
+}
+
+function showLogin() {
+    $(".loginTitle").click(function() {
+        $("#login").show();
+        $("#signup").hide();
+    });
+}
