21a7c190420089bce30ba1e143c3576ec6a72164
subasri19 <subasrigovindarajan@gmail.com>
2018-08-06 11:33:16 +0530

    Added form validation

---
+++ Page 1/signup.js
@@ -0,0 +1,50 @@
+$(document).ready(function() {
+    $("#signup").hide();
+    $("h1").lettering();
+});
+
+function showSignup() {
+    $(".signupTitle").click(function() {
+        $("#login").hide();
+        $("#signup").show();
+    });
+}
+
+function showLogin() {
+    $(".loginTitle").click(function() {
+        $("#login").show();
+        $("#signup").hide();
+    });
+}
+
+function loginValidate() {
+    var name = document.getElementById("usrname").value;
+    var psw = document.getElementById("psw").value;
+    if (name == "" || psw == "")
+        alert("Please don't leave any column blank");
+}
+
+function signupValidate() {
+    var name = document.getElementById("name").value;
+    var usrname = document.getElementById("usrname1").value;
+    var psw = document.getElementById("psw1").value;
+    var psw1 = document.getElementById("psw2").value;
+    var phno = document.getElementById("phno").value;
+    var mail = document.getElementById("mail").value;
+
+    if (name = "" || usrname == "" || psw == "" || psw1 == "" || phno == "" || mail == "")
+        alert("Please make sure if all the columns are filled");
+
+    var pswPattern = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/;
+    var phnoPattern = new RegExp("^[0-9]{10}$");
+    var mailidPattern = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+    if (!pswPattern.test(psw1) && !pswPattern.test(psw))
+        alert("Your password must comprise of numbers, spl characters, upper and lowercase letters with length between  6 and 16");
+    if (psw1 != psw)
+        alert("Your passwords doesn't match");
+    if (!phnoPattern.test(phno))
+        alert("Please enter a 10 digit phone number");
+    if (!mailidPattern.test(mail))
+        alert("Please enter a valid mail id");
+}
